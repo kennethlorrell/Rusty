4bade401e5b9a0aad66041fa9f7094baddaff5ae
Max <39443507+kennethlorrell@users.noreply.github.com>
2024-12-29 05:52:32 +0200

    Initialize new project

---
+++ hello_world/target/debug/Labs.d
@@ -0,0 +1,1 @@
+/home/phoe7ix/www/practice/Rust/Labs/hello_world/target/debug/Labs: /home/phoe7ix/www/practice/Rust/Labs/hello_world/src/main.rs
---
+++ hello_world/target/debug/deps/Labs-2be2dc77b0db4976.d
@@ -0,0 +1,5 @@
+/home/phoe7ix/www/practice/Rust/Labs/target/debug/deps/Labs-2be2dc77b0db4976: src/main.rs
+
+/home/phoe7ix/www/practice/Rust/Labs/target/debug/deps/Labs-2be2dc77b0db4976.d: src/main.rs
+
+src/main.rs:
